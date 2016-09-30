8818625c09b35c87073d34bc2117fad6b5b7832d
seven332 <seven332@163.com>
2016-09-30 19:29:19 +0800

    Add AutoSource

---
+++ library/src/main/java/com/hippo/largeimageview/AutoSource.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2016 Hippo Seven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hippo.largeimageview;
+
+/*
+ * Created by Hippo on 9/30/2016.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.hippo.streampipe.InputStreamPipe;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+public class AutoSource extends WrapperSource {
+
+    private static final String LOG_TAG = AutoSource.class.getSimpleName();
+
+    private boolean mInit;
+    private int mMaxBitmapSize;
+    private InitTask mTask;
+    private InputStreamPipe mPipe;
+
+    public AutoSource(@NonNull InputStreamPipe pipe) {
+        mPipe = pipe;
+    }
+
+    @Override
+    protected void onInit() {
+        mInit = true;
+        initSource();
+    }
+
+    @Override
+    public void setMaxBitmapSize(int maxSize) {
+        super.setMaxBitmapSize(maxSize);
+        mMaxBitmapSize = maxSize;
+        initSource();
+    }
+
+    public void initSource() {
+        if (!mInit || mMaxBitmapSize == 0 || mTask != null || isReady()) {
+            return;
+        }
+        mTask = new InitTask(this);
+        mTask.execute();
+    }
+
+    private void onInitDone(ImageSource imageSource) {
+        mTask = null;
+        if (imageSource != null) {
+            setImageSource(imageSource);
+        } else {
+            callSelfFailed();
+        }
+    }
+
+    @Override
+    public void recycle() {
+        super.recycle();
+        if (mTask != null) {
+            mTask.cancel(false);
+            mTask = null;
+        }
+        mPipe = null;
+    }
+
+    /**
+     * Decode InputStreamPipe to ImageSource.
+     * Called in non-UI thread.
+     */
+    protected ImageSource decode() {
+        final InputStreamPipe pipe = mPipe;
+        if (pipe == null) {
+            return null;
+        }
+
+        try {
+            pipe.obtain();
+
+            // Decode image info
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(pipe.open(), null, options);
+            pipe.close();
+            if (options.outWidth <= 0 || options.outHeight <= 0) {
+                // It is not a image
+                return null;
+            }
+
+            if (options.outWidth <= mMaxBitmapSize && options.outHeight <= mMaxBitmapSize) {
+                // BitmapSource
+                options.inJustDecodeBounds = false;
+                try {
+                    final Bitmap bitmap = BitmapFactory.decodeStream(pipe.open(), null, options);
+                    return new BitmapSource(bitmap);
+                } catch (OutOfMemoryError e) {
+                    return null;
+                }
+            } else {
+                // TiledBitmapSource
+                final BitmapRegionDecoder decoder = BitmapRegionDecoder.newInstance(pipe.open(), false);
+                if (decoder != null) {
+                    return new TiledBitmapSource(new SkiaRegionDecoder(decoder, Bitmap.Config.ARGB_8888));
+                } else {
+                    // Can't decode
+                    return null;
+                }
+            }
+        } catch (IOException e) {
+            return null;
+        } finally {
+            pipe.close();
+            pipe.release();
+            mPipe = null;
+        }
+    }
+
+    private static class InitTask extends AsyncTask<Void, Void, ImageSource> {
+
+        private final WeakReference<AutoSource> mAutoSource;
+
+        public InitTask(AutoSource autoSource) {
+            mAutoSource = new WeakReference<>(autoSource);
+        }
+
+        @Override
+        protected ImageSource doInBackground(Void... params) {
+            final AutoSource autoSource = mAutoSource.get();
+            if (autoSource != null) {
+                return autoSource.decode();
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ImageSource imageSource) {
+            final AutoSource autoSource = mAutoSource.get();
+            if (autoSource == null) {
+                Log.w(LOG_TAG, "Should call cancel() on InitTask");
+                if (imageSource != null) {
+                    imageSource.recycle();
+                }
+            } else {
+                autoSource.onInitDone(imageSource);
+            }
+        }
+
+        @Override
+        protected void onCancelled(ImageSource imageSource) {
+            if (imageSource != null) {
+                imageSource.recycle();
+            }
+        }
+    }
+}
--- library/src/main/java/com/hippo/largeimageview/WrapperSource.java
+++ library/src/main/java/com/hippo/largeimageview/WrapperSource.java
@@ -23,10 +23,40 @@ package com.hippo.largeimageview;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.support.annotation.NonNull;
 
-public abstract class WrapperSource extends ImageSource {
+public abstract class WrapperSource extends ImageSource implements ImageSource.Callback{
 
     private ImageSource mBase;
+    private int mWindowWidth;
+    private int mWindowHeight;
+    private int mMaxBitmapSize;
+
+    /**
+     * Set base ImageSource.
+     * Only call it once!
+     */
+    protected void setImageSource(@NonNull ImageSource base) {
+        if (mBase != null) {
+            throw new IllegalStateException("Can't set ImageSource twice");
+        }
+        mBase = base;
+        base.setCallback(this);
+        base.setWindowSize(mWindowWidth, mWindowHeight);
+        base.setMaxBitmapSize(mMaxBitmapSize);
+        // Ready
+        callSelfReady();
+    }
+
+    @Override
+    public void init() {
+        onInit();
+    }
+
+    /**
+     * Start point to init base ImageSource.
+     */
+    protected abstract void onInit();
 
     @Override
     public boolean isReady() {
@@ -35,31 +65,96 @@ public abstract class WrapperSource extends ImageSource {
 
     @Override
     public void setWindowSize(int w, int h) {
-
+        if (mBase != null) {
+            mBase.setWindowSize(w, h);
+        } else {
+            mWindowWidth = w;
+            mWindowHeight = h;
+        }
     }
 
     @Override
     public void setMaxBitmapSize(int maxSize) {
+        if (mBase != null) {
+            mBase.setMaxBitmapSize(maxSize);
+        } else {
+            mMaxBitmapSize = maxSize;
+        }
+    }
 
+    @Override
+    public void onAnimatorStart() {
+        if (mBase != null) {
+            mBase.onAnimatorStart();
+        }
+    }
+
+    @Override
+    public void onAnimatorEnd() {
+        if (mBase != null) {
+            mBase.onAnimatorEnd();
+        }
     }
 
     @Override
     public int getWidth() {
-        return 0;
+        if (mBase != null) {
+            return mBase.getWidth();
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        if (mBase != null) {
+            return mBase.getHeight();
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public void draw(Canvas canvas, Rect src, RectF dst) {
-
+        if (mBase != null) {
+            mBase.draw(canvas, src, dst);
+        }
     }
 
     @Override
     public void recycle() {
+        if (mBase != null) {
+            mBase.recycle();
+        }
+    }
 
+
+    ////////////////////
+    // Callback for base image source
+    ////////////////////
+
+    @Override
+    public void onImageReady(@NonNull ImageSource who) {
+        callSelfReady();
+    }
+
+    @Override
+    public void onImageFailed(@NonNull ImageSource who) {
+        callSelfFailed();
+    }
+
+    @Override
+    public void invalidateImage(@NonNull ImageSource who) {
+        invalidateSelf();
+    }
+
+    @Override
+    public void scheduleImage(@NonNull ImageSource who, @NonNull Runnable what, long when) {
+        scheduleSelf(what, when);
+    }
+
+    @Override
+    public void unscheduleImage(@NonNull ImageSource who, @NonNull Runnable what) {
+        unscheduleSelf(what);
     }
 }
